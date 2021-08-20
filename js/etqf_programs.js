6249d0aa0121437fa845839f0450c9b947c6b8cf
Ignasi Ribó <iribolabastida@gmail.com>
2021-08-20 23:29:08 +0700

    Create etqf_programs.js

---
+++ js/etqf_programs.js
@@ -0,0 +1,197 @@
+const programs = {
+  "633100601": {
+    "title_en": "Bachelor of Arts in English",
+    "title_th": "ศิลปศาสตรบัณฑิต (ภาษาอังกฤษ)",
+    "curriculum": { 
+      "1": {
+        "domain": "Ethics and Integrity",
+        "outcomes": {
+          "1.1": "Be able to perform assigned tasks with diligence, perseverance, and discipline.",
+          "1.2": "Be able to perform assigned tasks without violating academic ethics.",
+          "1.3": "Be able to apply the knowledge of English for the benefit of society in view of the common good."
+        }
+      },
+      "2": {
+        "domain": "Academic Achievement",
+        "outcomes": {
+          "2.1": "Be able to listen, speak, read, and write in English or Thai language on a variety of topics, summarizing key points from listening and reading, categorizing information accurately, and delivering effective reports both orally and in writing.",
+          "2.2": "Be able to analyze and describe the features and evolution of the English language, including different varieties of global English.",
+          "2.3": "Be able to read, analyze, and interpret literary works in prose, poetry, and drama, whether originally written in English or translated into English.",
+          "2.4": "Be able to explain the principles and theories of translation and to translate accurately and completely texts of various genres from English into Thai and from Thai into English.",
+          "2.5": "Be able to explain and discuss social, economic, political, educational, environmental, scientific, and technological issues in English." 
+        }
+      },
+      "3": {
+        "domain": "Intellectual and Cognitive Skills",
+        "outcomes": {
+          "3.1": "Be able to apply the knowledge of English in different contexts and to work effectively with the skills required in the 21st century.",
+          "3.2": "Be able to think critically, evaluate arguments rationally, and solve problems systematically.",
+          "3.3": "Be able to apply English to create innovations in various fields or to integrate the knowledge of linguistics with other sciences and disciplines.",
+          "3.4": "Be able to develop language innovations that benefit the community."
+        }   
+      },
+      "4": {
+        "domain": "Interpersonal Skills",
+        "outcomes": {
+          "4.1": "Be able to effectively collaborate with others, adapt to multicultural diversity, take into account other peoples' opinions, and develop leadership skills.",
+          "4.2": "Be able to apply the knowledge of English for personal development."
+        }
+      },
+      "5": {
+        "domain": "Mathematical Analysis, Communication, and Information Technology",
+        "outcomes": {
+          "5.1": "Be able to effectively present information by using modern technologies.",
+          "5.2": "Be able to use modern digital technologies to develop English language processes and improve work productivity.",
+          "5.3": "Be able to successfully communicate in English and/or in a third language."
+        }
+      }
+    }
+  },
+  "623100500": {
+    "title_en": "Bachelor of Arts in Thai Language and Culture for Foreigners",
+    "title_th": "ศิลปศาสตรบัณฑิต (สาขาวิชาภาษาและวัฒนธรรมไทยสำหรับชาวต่างประเทศ)",
+    "curriculum": { 
+      "1": {
+        "domain": "Ethics and Integrity",
+        "outcomes": {
+          "1.1": "Be able to perform assigned tasks with diligence, perseverance, and discipline.",
+          "1.2": "Be able to perform assigned tasks without violating academic ethics.",
+          "1.3": "Be able to apply the knowledge of English for the benefit of society in view of the common good."
+        }
+      },
+      "2": {
+        "domain": "Academic Achievement",
+        "outcomes": {
+          "2.1": "Be able to listen, speak, read, and write in English or Thai language on a variety of topics, summarizing key points from listening and reading, categorizing information accurately, and delivering effective reports both orally and in writing.",
+          "2.2": "Be able to analyze and describe the features and evolution of the English language, including different varieties of global English.",
+          "2.3": "Be able to read, analyze, and interpret literary works in prose, poetry, and drama, whether originally written in English or translated into English.",
+          "2.4": "Be able to explain the principles and theories of translation and to translate accurately and completely texts of various genres from English into Thai and from Thai into English.",
+          "2.5": "Be able to explain and discuss social, economic, political, educational, environmental, scientific, and technological issues in English." 
+        }
+      },
+      "3": {
+        "domain": "Intellectual and Cognitive Skills",
+        "outcomes": {
+          "3.1": "Be able to apply the knowledge of English in different contexts and to work effectively with the skills required in the 21st century.",
+          "3.2": "Be able to think critically, evaluate arguments rationally, and solve problems systematically.",
+          "3.3": "Be able to apply English to create innovations in various fields or to integrate the knowledge of linguistics with other sciences and disciplines.",
+          "3.4": "Be able to develop language innovations that benefit the community."
+        }   
+      },
+      "4": {
+        "domain": "Interpersonal Skills",
+        "outcomes": {
+          "4.1": "Be able to effectively collaborate with others, adapt to multicultural diversity, take into account other peoples' opinions, and develop leadership skills.",
+          "4.2": "Be able to apply the knowledge of English for personal development."
+        }
+      },
+      "5": {
+        "domain": "Mathematical Analysis, Communication, and Information Technology",
+        "outcomes": {
+          "5.1": "Be able to effectively present information by using modern technologies.",
+          "5.2": "Be able to use modern digital technologies to develop English language processes and improve work productivity.",
+          "5.3": "Be able to successfully communicate in English and/or in a third language."
+        }
+      }
+    }
+  },
+  "633100999": {
+    "title_en": "General Education",
+    "title_th": "",
+    "curriculum": { 
+      "1": {
+        "domain": "Ethics and Integrity",
+        "outcomes": {
+          "1.1": "Be able to perform assigned tasks with diligence, perseverance, and discipline.",
+          "1.2": "Be able to perform assigned tasks without violating academic ethics.",
+          "1.3": "Be able to apply the knowledge of English for the benefit of society in view of the common good."
+        }
+      },
+      "2": {
+        "domain": "Academic Achievement",
+        "outcomes": {
+          "2.1": "Be able to listen, speak, read, and write in English or Thai language on a variety of topics, summarizing key points from listening and reading, categorizing information accurately, and delivering effective reports both orally and in writing.",
+          "2.2": "Be able to analyze and describe the features and evolution of the English language, including different varieties of global English.",
+          "2.3": "Be able to read, analyze, and interpret literary works in prose, poetry, and drama, whether originally written in English or translated into English.",
+          "2.4": "Be able to explain the principles and theories of translation and to translate accurately and completely texts of various genres from English into Thai and from Thai into English.",
+          "2.5": "Be able to explain and discuss social, economic, political, educational, environmental, scientific, and technological issues in English." 
+        }
+      },
+      "3": {
+        "domain": "Intellectual and Cognitive Skills",
+        "outcomes": {
+          "3.1": "Be able to apply the knowledge of English in different contexts and to work effectively with the skills required in the 21st century.",
+          "3.2": "Be able to think critically, evaluate arguments rationally, and solve problems systematically.",
+          "3.3": "Be able to apply English to create innovations in various fields or to integrate the knowledge of linguistics with other sciences and disciplines.",
+          "3.4": "Be able to develop language innovations that benefit the community."
+        }   
+      },
+      "4": {
+        "domain": "Interpersonal Skills",
+        "outcomes": {
+          "4.1": "Be able to effectively collaborate with others, adapt to multicultural diversity, take into account other peoples' opinions, and develop leadership skills.",
+          "4.2": "Be able to apply the knowledge of English for personal development."
+        }
+      },
+      "5": {
+        "domain": "Mathematical Analysis, Communication, and Information Technology",
+        "outcomes": {
+          "5.1": "Be able to effectively present information by using modern technologies.",
+          "5.2": "Be able to use modern digital technologies to develop English language processes and improve work productivity.",
+          "5.3": "Be able to successfully communicate in English and/or in a third language."
+        }
+      }
+    }
+  },
+  "633100998": {
+    "title_en": "Foreign Languages",
+    "title_th": "",
+    "curriculum": { 
+      "1": {
+        "domain": "Ethics and Integrity",
+        "outcomes": {
+          "1.1": "Be able to perform assigned tasks with diligence, perseverance, and discipline.",
+          "1.2": "Be able to perform assigned tasks without violating academic ethics.",
+          "1.3": "Be able to apply the knowledge of English for the benefit of society in view of the common good."
+        }
+      },
+      "2": {
+        "domain": "Academic Achievement",
+        "outcomes": {
+          "2.1": "Be able to listen, speak, read, and write in English or Thai language on a variety of topics, summarizing key points from listening and reading, categorizing information accurately, and delivering effective reports both orally and in writing.",
+          "2.2": "Be able to analyze and describe the features and evolution of the English language, including different varieties of global English.",
+          "2.3": "Be able to read, analyze, and interpret literary works in prose, poetry, and drama, whether originally written in English or translated into English.",
+          "2.4": "Be able to explain the principles and theories of translation and to translate accurately and completely texts of various genres from English into Thai and from Thai into English.",
+          "2.5": "Be able to explain and discuss social, economic, political, educational, environmental, scientific, and technological issues in English." 
+        }
+      },
+      "3": {
+        "domain": "Intellectual and Cognitive Skills",
+        "outcomes": {
+          "3.1": "Be able to apply the knowledge of English in different contexts and to work effectively with the skills required in the 21st century.",
+          "3.2": "Be able to think critically, evaluate arguments rationally, and solve problems systematically.",
+          "3.3": "Be able to apply English to create innovations in various fields or to integrate the knowledge of linguistics with other sciences and disciplines.",
+          "3.4": "Be able to develop language innovations that benefit the community."
+        }   
+      },
+      "4": {
+        "domain": "Interpersonal Skills",
+        "outcomes": {
+          "4.1": "Be able to effectively collaborate with others, adapt to multicultural diversity, take into account other peoples' opinions, and develop leadership skills.",
+          "4.2": "Be able to apply the knowledge of English for personal development."
+        }
+      },
+      "5": {
+        "domain": "Mathematical Analysis, Communication, and Information Technology",
+        "outcomes": {
+          "5.1": "Be able to effectively present information by using modern technologies.",
+          "5.2": "Be able to use modern digital technologies to develop English language processes and improve work productivity.",
+          "5.3": "Be able to successfully communicate in English and/or in a third language."
+        }
+      }
+    }
+  }
+}
+
+
+if (typeof window === 'undefined') { module.exports = { programs } }
