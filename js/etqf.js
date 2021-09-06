98d52ed1ff58d5352339e2b02aef98e7d939e0f7
seliestel <iribolabastida@gmail.com>
2021-09-06 15:48:29 +0700

    Fixing problem with tqf5

--- js/etqf.js
+++ js/etqf.js
@@ -867,7 +867,6 @@ $(".upload_draft_eTQF5").on('change', function() {
       fillForm(jsonTQF.version, jsonTQF.form);
     } catch(err) {
       console.log(err);
-      hideModal();
       showModal([
         "Unable to proceed",
         "<p>The file you are trying to upload is not an eTQF5 (json) file or has been corrupted. Please try again or upload a valid eTQF3.</p>",
